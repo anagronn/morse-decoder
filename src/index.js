64ff1c87a1121ed838da1fbf4cd0b6873133c43b
anagronn <nastasya.vodchits@mail.ru>
2024-07-27 23:20:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,46 +1,87 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
+const REPLACEMENT = {
+  '10': '.',
+  '11': '-',
+}
+
 function decode(expr) {
-    // write your solution here
+  let words = expr.split('**********');
+
+  let morseWords = [];
+  words.forEach((word) => {
+    morseWords.push(morseWord(word));
+  })
+
+  return morseWords.map((word) => decodeWord(word)).join(' ');
+}
+
+function decodeWord(morseLetters) {
+  let decodedLetters = [];
+  morseLetters.forEach((letter) => {
+    for (let key in MORSE_TABLE) {
+      if (letter === key) {
+        decodedLetters.push(MORSE_TABLE[key]);
+      }
+    }
+  })
+  return decodedLetters.join('');
+}
+
+function morseWord(word) {
+  let morseLetters = []
+
+  word.split('').forEach((_, i) => {
+    if (i === 0 || i % 10 === 0) {
+      morseLetters.push(word.slice(i, i + 10));
+    }
+  });
+
+  return morseLetters.map((letter) => morseLetter(letter));
+}
+
+function morseLetter(letter) {
+  let morseLetter = letter.replace(/10|11/g, match => REPLACEMENT[match])
+  return morseLetter.replaceAll('0', '');
 }
 
 module.exports = {
-    decode
+  decode
 }
